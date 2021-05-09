ac46931e99a07843018ce70fab47c420a349c6d4
DBereznak <dbereznak@gmail.com>
2021-05-09 16:59:13 -0400

    Tweaking some styles

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const menu = document.getElementById("menu");
+const cardTable = document.getElementById("cards")
 
 const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", displayMobileMenu);
@@ -11,7 +12,30 @@ const menuItems = [
     {name: 'Contact', link: '#'}
 ];
 
+const cards = [
+    {title: "Justo Placerat", body: "Eu non-col commodo accumsan anti mi. Commodo consectetur sed mi adipiscing accumsan ac nunc tincidunt lobortis"},
+    {title: "Justo Placerat", body: "Eu non-col commodo accumsan anti mi. Commodo consectetur sed mi adipiscing accumsan ac nunc tincidunt lobortis"},
+    {title: "Justo Placerat", body: "Eu non-col commodo accumsan anti mi. Commodo consectetur sed mi adipiscing accumsan ac nunc tincidunt lobortis"},
+]
+
 buildMenu(menuItems);
+buildCards(cards)
+
+function buildCards(cards){
+ for(let card of cards){
+     let cardItem = document.createElement("div");
+     cardItem.classList.add("card");
+     let face = `
+     <div class="circle">
+   </div>
+   <h3>${card.title}</h3>
+   <p>
+    ${card.body}
+   </p>`;
+   cardItem.innerHTML = face;
+   cardTable.appendChild(cardItem);
+ }
+}
 
 function buildMenu(items){
     for(let item of items){
